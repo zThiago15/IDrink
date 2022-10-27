67b7c23895e99f40b9513b407fa46e408b4948ef
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-10-27 18:55:54 -0300

    feat: adicionando rotas da aplicacao

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Product from './pages/Product';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={ Login } />
+        <Route path="/register" element={ Register } />
+        <Route path="/customer/products" element={ Product } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
