c93863f1cd37c31c64faec245ddeb8938062c056
jey <andre.marqquez@gmail.com>
2022-10-28 12:51:09 -0300

    remove endpoint < health > /login

--- back-end/src/api/routes/loginRoute.js
+++ back-end/src/api/routes/loginRoute.js
@@ -3,10 +3,6 @@ const loginController = require('../controllers/loginController');
 
 const router = Router();
 
-router.get('/health', (_req, res) => {
-  res.status(200).json({ message: '/login Online' });
-});
-
 router.post('/', loginController.login);
 
 module.exports = router;
