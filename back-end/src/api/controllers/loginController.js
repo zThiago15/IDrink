3410449e20d9c0d30ac2c5a576b700cfc6bd6432
iskandarzero <riderzerotg@gmail.com>
2022-10-28 02:58:45 -0300

    feat: adds create function on loginController

--- back-end/src/api/controllers/loginController.js
+++ back-end/src/api/controllers/loginController.js
@@ -6,5 +6,13 @@ const loginController = {
     const login = await loginService.login({ email, password });
     res.status(200).json({ login });
   },
+  create: async (req, res) => {
+    const { name, email, password } = req.body;
+    const user = await loginService.create(name, email, password);
+
+    if (!user) return res.status(409).json({ message: 'Conflict' });
+
+    return res.status(201).json({ message: 'Created' });
+  }
 };
 module.exports = loginController;
