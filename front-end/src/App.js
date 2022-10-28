f71adeb601181403919790aa109eaccc5cb9708b
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-27 23:41:38 -0300

    fix: erro ao inserir element nas rotas

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -9,9 +9,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={ Login } />
-        <Route path="/register" element={ Register } />
-        <Route path="/customer/products" element={ Product } />
+        <Route path="/login" element={ <Login /> } />
+        <Route path="/register" element={ <Register /> } />
+        <Route path="/customer/products" element={ <Product /> } />
       </Routes>
     </BrowserRouter>
   );
