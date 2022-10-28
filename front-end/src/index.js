735c6ea07efd37cae9925fea4de201f4e2f84d2e
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-10-27 21:48:15 -0300

    feat: configurações iniciais do redux/toolkit

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
+import store from './redux/store';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={ store }>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
