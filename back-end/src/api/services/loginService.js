5aabacedf1da86d95f0e43d2d882a6c0153cc717
iskandarzero <riderzerotg@gmail.com>
2022-10-28 02:58:32 -0300

    feat: adds create function on loginService

--- back-end/src/api/services/loginService.js
+++ back-end/src/api/services/loginService.js
@@ -1,13 +1,27 @@
-const { loginModel } = require('../../database/models');
+const { User } = require('../../database/models');
 const ErrorBadRequest = require('../errors/ErrorBadRequest');
+const { Op } = require("sequelize")
+var md5 = require('md5');
 
 const loginService = {
   login: async ({ email, password }) => {
-    const user = await loginModel.findOne({ where: { email } });
-    console.log(user);
+    const user = await User.findOne({ where: { email } });
+
     if (!user) throw new ErrorBadRequest('Invalid fields');
     return password;
   },
+  create: async (name, email, password ) => {
+    const user = await User.findOne({ where: { [Op.or]: [{ name }, { email }] } });
+
+    if (user) return false;
+
+    const hashedPassword = md5(password);
+    const newUser = await User.create(
+      {name, email, password: hashedPassword, role: 'customer'}
+    );
+
+    return newUser;
+  }
 };
 
 module.exports = loginService;
