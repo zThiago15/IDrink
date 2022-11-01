bc58c46fd0a9a35297867c0abc694ba074c24762
jey <andre.marqquez@gmail.com>
2022-11-01 10:23:12 -0300

    fix [jwt]:Altera jwt secret de env para leitura

--- back-end/src/api/middlewares/verifyTokenJWT.js
+++ back-end/src/api/middlewares/verifyTokenJWT.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken');
+const fs = require('fs');
 require('express-async-errors');
 
-const { JWT_SECRET } = process.env;
+const JWT_SECRET = fs.readFileSync("jwt.evaluation.key", "utf-8");
 
 module.exports = async (req, res, next) => {
   const token = req.headers.authorization;
