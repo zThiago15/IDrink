e0c1c4b0ac55d384c993dd536f223b3b9093b5d0
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-11-02 12:27:14 -0300

    refactor: componentizado elemento CarShop de checkout

--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -1,24 +1,10 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { selectProduct, actionRemoveItem } from '../redux/userProducts';
 import NavBar from '../components/Navbar';
+import CarShop from '../components/CarShop';
 
 export default function Checkout() {
-  const products = useSelector(selectProduct);
-  const dispatch = useDispatch();
   const navigate = useNavigate();
-  const totalPrice = () => {
-    let total = 0;
-    products.forEach((product) => {
-      total += product.price * product.quantity;
-    });
-    return total.toFixed(2).replace('.', ',');
-  };
-
-  const removeItemInShopCar = (index) => {
-    dispatch(actionRemoveItem(index));
-  };
 
   const finalizedBuy = () => {
     navigate('customer/orders/1');
@@ -29,61 +15,7 @@ export default function Checkout() {
       <NavBar />
       <h1>Checkout</h1>
       <p>Finalizar Pedido</p>
-      <table>
-        <tr>
-          <td>Item</td>
-          <td>Descrição</td>
-          <td>Quantidade</td>
-          <td>Valor unitário</td>
-          <td>Subtotal</td>
-          <td>Remover Item</td>
-        </tr>
-        {products.map((product, index) => (
-          <tr key={ index }>
-            <td
-              data-testid={
-                `customer_checkout__element-order-table-item-number-${index}`
-              }
-            >
-              {index + 1}
-            </td>
-            <td
-              data-testid={ `customer_checkout__element-order-table-name-${index}` }
-            >
-              {product.name}
-            </td>
-            <td
-              data-testid={ `customer_checkout__element-order-table-quantity-${index}` }
-            >
-              {product.quantity}
-            </td>
-            <td
-              data-testid={ `customer_checkout__element-order-table-unit-price-${index}` }
-            >
-              {product.price.replace('.', ',')}
-            </td>
-            <td
-              data-testid={ `customer_checkout__element-order-table-sub-total-${index}` }
-            >
-              {String((product.quantity * product.price).toFixed(2)).replace('.', ',')}
-            </td>
-            <button
-              data-testid={ `customer_checkout__element-order-table-remove-${index}` }
-              type="button"
-              onClick={ () => removeItemInShopCar(index) }
-            >
-              Remover
-
-            </button>
-          </tr>
-        ))}
-      </table>
-      <h2
-        data-testid="customer_checkout__element-order-total-price"
-      >
-        {`Total: R$ ${totalPrice()}`}
-
-      </h2>
+      <CarShop />
 
       <div>
         <h3>Detalhes e Endereços para Entrega</h3>
