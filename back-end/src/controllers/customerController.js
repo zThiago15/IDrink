39a3c957c800096f85f94722f078cfa1b35d69de
jey <andre.marqquez@gmail.com>
2022-11-03 17:05:49 -0300

    comenta service customer

--- back-end/src/controllers/customerController.js
+++ back-end/src/controllers/customerController.js
@@ -1,4 +1,4 @@
-const customerService = require('../services/customerService');
+// const customerService = require('../services/customerService');
 
 const getAllSales = async (_req, res) => {
   return res.status(200).json({ message: 'rota customer' });
