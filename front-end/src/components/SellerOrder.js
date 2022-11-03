ee9275bd3049352e1f3596a311fb7ad61a66c748
iskandarzero <riderzerotg@gmail.com>
2022-11-03 15:12:27 -0300

    fix: change file name

--- front-end/src/components/SellerOrder.js
+++ front-end/src/components/SellerOrder.js
@@ -2,17 +2,19 @@ import PropTypes from 'prop-types';
 
 export default function SellerOrder(props) {
   const { order } = props;
-  const { id, status, data, preco, endereco } = order;
+  const { id, status, saleDate, totalPrice, deliveryAddress, deliveryNumber } = order;
 
   return (
     <div>
       <p data-testid={ `seller_orders__element-order-id-${id}` }>{`Pedido ${id}`}</p>
       <div data-testid={ `seller_orders__element-delivery-status-${id}` }>{status}</div>
       <div>
-        <p data-testid={ `seller_orders__element-order-date-${id}` }>{data}</p>
-        <p data-testid={ `seller_orders__element-card-price-${id}` }>{preco}</p>
+        <p data-testid={ `seller_orders__element-order-date-${id}` }>{saleDate}</p>
+        <p data-testid={ `seller_orders__element-card-price-${id}` }>{totalPrice}</p>
       </div>
-      <p data-testid={ `seller_orders__element-card-address-${id}` }>{endereco}</p>
+      <p data-testid={ `seller_orders__element-card-address-${id}` }>
+        {`${deliveryAddress}, ${deliveryNumber}`}
+      </p>
     </div>
   );
 }
