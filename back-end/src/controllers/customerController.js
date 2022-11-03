85904862a7b5891f29ab73ec041c2948f8eb7197
jey <andre.marqquez@gmail.com>
2022-11-03 17:35:09 -0300

    add singlequote

--- back-end/src/controllers/customerController.js
+++ back-end/src/controllers/customerController.js
@@ -1,8 +1,6 @@
 // const customerService = require('../services/customerService');
 
-const getAllSales = async (_req, res) => {
-  return res.status(200).json({ message: 'rota customer' });
-};
+const getAllSales = async (_req, res) => res.status(200).json({ message: 'rota customer' });
 
 module.exports = {
   getAllSales,
