ea63b31857bf37ce9a29d46f09587bd96b86e2c8
iskandarzero <riderzerotg@gmail.com>
2022-11-03 15:13:19 -0300

    fix: change file format

---
+++ front-end/src/pages/SellerOrders.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import getOrders from '../services/orders';
+import SellerOrder from '../components/SellerOrder';
+import Navbar from '../components/Navbar';
+
+export default function SellerOrders() {
+  const [orders, setOrders] = useState();
+  useEffect(() => {
+    const saveOrders = async () => {
+      const data = await getOrders();
+
+      setOrders(data);
+    };
+    saveOrders();
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      {orders
+      && orders.map((order, index) => <SellerOrder key={ index } order={ order } />)}
+    </div>
+  );
+}
