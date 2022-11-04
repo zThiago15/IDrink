362942bc5f4647796115efde9bf9f9b66968061d
iskandarzero <riderzerotg@gmail.com>
2022-11-04 14:33:50 -0300

    feat: createSale controller

--- back-end/src/controllers/customerController.js
+++ back-end/src/controllers/customerController.js
@@ -1,7 +1,16 @@
-// const customerService = require('../services/customerService');
+const customerService = require('../services/customerService');
+
+const createSale = async (req, res) => {
+  const saleInfo = req.body;
+  const user = req.user;
+  const sale = await customerService.createSale(user, saleInfo);
+
+  return res.status(201).json(sale.id);
+}
 
 const getAllSales = async (_req, res) => res.status(200).json({ message: 'rota customer' });
 
 module.exports = {
   getAllSales,
+  createSale
 };
