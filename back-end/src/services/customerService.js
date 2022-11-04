eee7b8901c6278f889991469217a65555bdb4b62
iskandarzero <riderzerotg@gmail.com>
2022-11-04 14:33:39 -0300

    feat: createSale service

--- back-end/src/services/customerService.js
+++ back-end/src/services/customerService.js
@@ -1,5 +1,30 @@
-const { SalesModel } = require('../database/models');
+const { SalesModel, UserModel, SalesProductsModel, ProductsModel } = require('../database/models');
 require('express-async-errors');
+
+const createSale = async (user, sale) => {
+  const seller = await UserModel.findOne({ where: { name: sale.seller } });
+
+  const response = await SalesModel.create({
+    userId: user.id,
+    sellerId: seller.id,
+    totalPrice: sale.totalPrice,
+    deliveryAddress: sale.deliveryAddress,
+    deliveryNumber: sale.deliveryNumber,
+    status: 'Pendente'
+  });
+  
+  Promise.all(sale.items.map(async (item) => {
+    const product = await ProductsModel.findOne({where: { name: item.name }});
+
+    SalesProductsModel.create({
+      saleId: response.id,
+      productId: product.id,
+      quantity: item.quantity,
+    })
+  }));
+
+  return response;
+}
 
 const getAllSales = async () => {
   const response = await SalesModel.findAll();
@@ -8,4 +33,5 @@ const getAllSales = async () => {
 
 module.exports = {
   getAllSales,
+  createSale
 };
