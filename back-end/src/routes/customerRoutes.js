f6408e4922fb10e1f3dd6545b6e6f7d254a4f84d
iskandarzero <riderzerotg@gmail.com>
2022-11-04 14:34:06 -0300

    feat: createSale route

--- back-end/src/routes/customerRoutes.js
+++ back-end/src/routes/customerRoutes.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const customerController = require('../controllers/customerController');
+const verifyToken = require('../middlewares/verifyTokenJWT');
 
 const router = Router();
 
 router.get('/', customerController.getAllSales);
+router.post('/', verifyToken, customerController.createSale);
 
 module.exports = router;
