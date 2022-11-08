8901cd0d8be7cf7050cbd7df7fba44fedae53b37
juniorMira <j-unyor14@hotmail.com>
2022-11-08 15:06:40 -0300

    refactor button register

--- front-end/src/components/AdministratorInput.jsx
+++ front-end/src/components/AdministratorInput.jsx
@@ -1,105 +1,106 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import adminCreateUser from '../helpers/AdmCreate';
 import dataTestIds from '../utils/dataTestIds';
+import NavBarAdm from './navBarAdm';
 
 export default function RegisterForm() {
   const [userName, setUserName] = useState('');
   const [userEmail, setUserEmail] = useState('');
   const [userPassword, setUserPassword] = useState('');
   const [role, setRole] = useState('');
-  const MIN_LENGTH_PASSWORD = 6;
-  const MIN_LENGTH_NAME = 12;
+  const [error, setError] = useState(false);
 
-  const validateEmail = (email) => {
-    const regex = /\S+@\S+\.\S+/;
-    return regex.test(email);
+  const validateForm = () => {
+    const MIN_LENGTH_PASSWORD = 6;
+    const MIN_LENGTH_NAME = 12;
+    const isNameValid = userName.length >= MIN_LENGTH_NAME;
+    const emailRegex = /^[a-z0-9._]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+    // https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail
+    const isEmailValid = emailRegex.test(userEmail);
+    const isPasswordValid = userPassword.length >= MIN_LENGTH_PASSWORD;
+    return !(isEmailValid && isPasswordValid && isNameValid);
   };
 
-  const validateForm = () => !(
-    userName.length >= MIN_LENGTH_NAME
-    && validateEmail(userEmail)
-    && userPassword.length >= MIN_LENGTH_PASSWORD
-    && role !== ''
-  );
-
-  buttonRegister = async () => {
-    const url = 'http://localhost:3001/admin/manage';
-    const data = JSON.parse(localStorage.getItem('user'));
-    const request = {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        Authorization: data.token,
-      },
-      body: JSON.stringify({
-        name: userName,
-        email: userEmail,
-        password: userPassword,
-        role,
-      }),
-    };
-    await fetch(url, request)
-  }
+  const buttonRegister = async (event) => {
+    const CONFLICT = 409;
+    event.preventDefault();
+    try {
+      const { token } = JSON.parse(localStorage.getItem('user'));
+      const newUser = await adminCreateUser({ email, password, userName, role }, token);
+      if (Number(newUser.status) === CONFLICT) {
+        setError(true);
+      }
+    } catch (err) {
+      setError(true);
+    }
+  };
 
   return (
     <div>
-      <label htmlFor='name'>
-        Nome
-        <input
-        data-testid={ `${dataTestIds[64]}` }
-        placeholder='Nome'
-        type='text'
-        id='name'
-        onChange={ ({ target }) => setUserName(target.value)}
-        />
-      </label>
+      <NavBarAdm />
+      {
+        error && <span data-testid={ dataTestIds[74] }>Usuário já existe!</span>
+      }
+      <form onSubmit={ buttonRegister }>
+        <label htmlFor="name">
+          Nome
+          <input
+            data-testid={ `${dataTestIds[64]}` }
+            placeholder="Nome"
+            type="text"
+            id="name"
+            value={ userName }
+            onChange={ ({ target }) => setUserName(target.value) }
+          />
+        </label>
 
-      <label htmlFor='email'>
-        Login
-        <input
-        data-testid={ `${dataTestIds[65]}`}
-        placeholder='Ex: your@email.com'
-        type='email'
-        id='email'
-        onChange={ ({ target }) => setUserEmail(target.value)}
-        />
-      </label>
+        <label htmlFor="email">
+          Login
+          <input
+            data-testid={ `${dataTestIds[65]}` }
+            placeholder="Ex: your@email.com"
+            type="email"
+            id="email"
+            value={ userEmail }
+            onChange={ ({ target }) => setUserEmail(target.value) }
+          />
+        </label>
 
-      <label htmlFor='password'>
-        Senha
-        <input
-        data-testid={ `${dataTestIds[66]}`}
-        placeholder='******'
-        type='password'
-        id='password'
-        onChange={ ({ target }) => setUserPassword(target.value)}
-        />
-      </label>
+        <label htmlFor="password">
+          Senha
+          <input
+            data-testid={ `${dataTestIds[66]}` }
+            placeholder="******"
+            type="password"
+            id="password"
+            value={ userPassword }
+            onChange={ ({ target }) => setUserPassword(target.value) }
+          />
+        </label>
 
-      <label htmlFor='user-type'>
-        Tipo
-        <select
-        id='user-type'
-        data-testid={ `${dataTestIds[68]}`}
-        defaultValue=''
-        onChange={ ({ target }) => setRole(target.value)}
+        <label htmlFor="user-type">
+          Tipo
+          <select
+            id="user-type"
+            data-testid={ `${dataTestIds[68]}` }
+            value={ role }
+            onChange={ ({ target }) => setRole(target.value) }
+          >
+            <option value="" disabled hidden>Escolha o tipo</option>
+            <option value="custumer">Consumidor</option>
+            <option value="seller">Vendedor</option>
+            <option value="administrator">Administrador</option>
+          </select>
+        </label>
+
+        <button
+          type="submit"
+          data-testid={ `${dataTestIds[67]}` }
+          disabled={ validateForm() }
         >
-          <option value='' disabled hidden>Escolha o tipo</option>
-          <option value='custumer'>Consumidor</option>
-          <option value='seller'>Vendedor</option>
-          <option value='administrator'>Administrador</option>
-        </select>
-      </label>
-
-      <button
-      type='button'
-      data-testid={ `${dataTestIds[67]}`}
-      disabled={ validateForm() }
-      onClick={ buttonRegister }
-      >
-        Cadastrar
-      </button>
+          Cadastrar
+        </button>
+      </form>
     </div>
-  )
+  );
 }
