5bddebbce151cc8e183a3afb33dce3f568fe91c9
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-11-09 18:39:04 -0300

    test: mockando vaapi que retorna produtos e localStorage

---
+++ front-end/src/tests/mocks/product.mock.js
@@ -0,0 +1,19 @@
+const products = [{
+  id: 1,
+  name: 'Skol 200ml',
+  urlImage: 'http://localhost:3001/img1',
+  price: 1.25,
+}, {
+  id: 1,
+  name: 'Brahma',
+  urlImage: 'http://localhost:3001/img2',
+  price: 5.00,
+},
+{
+  id: 1,
+  name: 'Skol 200ml',
+  urlImage: 'http://localhost:3001/img3',
+  price: 3.00,
+}];
+
+export default products;
