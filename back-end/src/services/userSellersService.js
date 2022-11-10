5c2260eeafede31fff29601845a87882d8b428ca
jey <andre.marqquez@gmail.com>
2022-11-10 14:49:36 -0300

    fix lint

--- back-end/src/services/userSellersService.js
+++ back-end/src/services/userSellersService.js
@@ -5,8 +5,8 @@ require('express-async-errors');
 const userSellersService = {
   getUserSalles: async () =>
     UserModel.findAll({
-      where: { role: "seller" },
-      attributes: ["id", "name"],
+      where: { role: 'seller' },
+      attributes: ['id', 'name'],
     }),
 
   getAllOrders: async (sellerId) => {
@@ -16,17 +16,10 @@ const userSellersService = {
     const orders = response.reduce(
       (
         acc,
-        { id, status, saleDate, totalPrice, deliveryAddress, deliveryNumber }
+        { id, status, saleDate, totalPrice, deliveryAddress, deliveryNumber },
       ) =>
-        acc.concat({
-          id,
-          status,
-          saleDate,
-          totalPrice,
-          deliveryAddress,
-          deliveryNumber,
-        }),
-      []
+        acc.concat({ id, status, saleDate, totalPrice, deliveryAddress, deliveryNumber }),
+      [],
     );
     return orders;
   },
@@ -39,7 +32,7 @@ const userSellersService = {
         where: {
           id: orderId,
         },
-      }
+      },
     );
     const newOrder = await customerService.getOrder(orderId);
     return newOrder;
