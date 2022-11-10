74935461c089bed94e4158e5123c1f22e34f99ea
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-11-10 10:13:55 -0300

    test: teste que verifica o funcionamento da page de checkout

--- front-end/src/tests/mocks/customer.mock.js
+++ front-end/src/tests/mocks/customer.mock.js
@@ -2,23 +2,31 @@
 export const mockUserStore = {
   name: 'Fulana Pereira',
   email: 'fulana@deliveryapp.com',
-  role: 'seller',
+  role: 'customer',
   token: 'eyJhbGciOiJIUzI1NiIA8-6HB5LxgqQlGwCDZCMcEj2ZVQ' };
+
+export const mockseller = { data: [{
+  id: 2,
+  name: 'Fulana Pereira',
+}] };
 
 export const mockProducts = [
   {
+    id: 1,
     name: 'Skol Lata 250ml',
     price: '2.20',
-    quantity: 2,
+    url: 'http://localhost:3001/images/skol_lata_350ml.jpg',
   },
   {
+    id: 2,
+    name: 'Heineken 600ml',
+    price: '7.50',
+    url: 'http://localhost:3001/images/heineken_600ml.jpg',
+  },
+  {
+    id: 3,
     name: 'Antarctica Pilsen 300ml',
     price: '2.49',
-    quantity: 2,
-  },
-  {
-    name: 'Brahma 600ml',
-    price: '7.50',
-    quantity: 2,
+    url: 'http://localhost:3001/images/antarctica_pilsen_300ml.jpg',
   },
 ];
